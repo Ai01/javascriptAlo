3aa6741f78d61c1e3c8690ff07f460c7c5c3f604
柏海辉 <baihaihui@bohaihuideMacBook-Pro.local>
2017-12-14 18:39:21 +0800

    feature:deque的基本实现

---
+++ deque/src/deque.js
@@ -0,0 +1,48 @@
+// 双向队列
+
+class Deque {
+  constructor(array) {
+    this.data = [];
+    if (Array.isArray(array)) {
+      this.data = [].concat(array);
+    }
+  }
+
+  addFirst(e) {
+    this.data.unshift(e);
+  }
+
+  removeFirst() {
+    return this.data.shift();
+  }
+
+  getFirst() {
+    return this.data[0];
+  }
+
+  addLast(e) {
+    this.data.push(e);
+  }
+
+  removeLast() {
+    return this.data.pop();
+  }
+
+  getLast() {
+    return this.data[this.data.length - 1];
+  }
+
+  length() {
+    return this.data.length;
+  }
+
+  clean() {
+    this.data = [];
+  }
+
+  peek(i) {
+    return this.data[i];
+  }
+}
+
+module.exports = Deque;
---
+++ deque/src/test.js
@@ -0,0 +1,51 @@
+// deque测试
+
+const Deque = require('./deque');
+
+const underline = () => {
+  console.log('---------------------');
+};
+
+console.log('constructor测试:');
+const deque = new Deque([4, 1, 2]);
+console.log(deque);
+
+underline();
+
+console.log('addFirst测试');
+deque.addFirst(5);
+console.log(deque);
+
+underline();
+
+console.log('removeFirst测试');
+deque.removeFirst();
+console.log(deque);
+
+underline();
+
+console.log('addLast测试');
+deque.addLast(8);
+console.log(deque);
+
+underline();
+
+console.log('removeLast测试');
+deque.removeLast();
+console.log(deque);
+
+underline();
+
+console.log('peek测试');
+console.log(deque.peek(2));
+
+underline();
+
+console.log('length测试');
+console.log(deque.length());
+
+underline();
+
+console.log('clean测试');
+deque.clean();
+console.log(deque);
