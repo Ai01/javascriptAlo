58b6b1b93a8a134ef13cd45581de30ab630851ba
柏海辉 <baihaihui@ACA8015F.ipt.aol.com>
2018-12-13 14:34:57 +0800

    note: 背包问题

---
+++ 基本功与思想/分治/动态规划/背包问题/knapsack.js
@@ -0,0 +1,36 @@
+const { consoleTree } = require('../../../../utils/consoleUtils');
+
+
+// f[i][v]：表示前i件物品恰放入一个容量为v的背包可以获得的最大价值。
+// 状态转移方程是：f[i][v]=max{f[i-1][v],f[i-1][v-weight[i]]+value[i]}
+function knapsack(weights, values, W) {
+  const n = weights.length - 1;
+  const f = [[]];
+  for (let j = 0; j <= W; j++) {
+    if (j < weights[0]) {
+      //如果容量不能放下物品0的重量，那么价值为0
+      f[0][j] = 0;
+    } else {
+      //否则等于物体0的价值
+      f[0][j] = values[0];
+    }
+  }
+  for (let j = 0; j <= W; j++) {
+    for (let i = 1; i <= n; i++) {
+      if (!f[i]) {
+        //创建新一行
+        f[i] = [];
+      }
+      if (j < weights[i]) {
+        //等于之前的最优值
+        f[i][j] = f[i - 1][j];
+      } else {
+        f[i][j] = Math.max(f[i - 1][j], f[i - 1][j - weights[i]] + values[i]);
+      }
+    }
+  }
+  consoleTree(f);
+  return f[n][W];
+}
+const a = knapsack([2, 2, 6, 5, 4], [6, 3, 5, 4, 6], 10);
+console.log(a);
