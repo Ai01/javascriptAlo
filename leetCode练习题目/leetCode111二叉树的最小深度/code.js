003e86216be53ae62aaf283fc026181f5ec91bec
柏海辉 <baihaihui@ACA80121.ipt.aol.com>
2019-01-22 16:10:39 +0800

    note: 二叉树的最小深度

--- leetCode练习题目/leetCode111二叉树的最小深度/code.js
+++ leetCode练习题目/leetCode111二叉树的最小深度/code.js
@@ -1,3 +1,6 @@
+const Queue = require('../../数据结构/队列/src/queue');
+
+// 递归实现
 var minDepth = function(root) {
 
   const helper = (node, path) => {
@@ -11,4 +14,35 @@ var minDepth = function(root) {
   }
 
   return helper(root, 0);
+};
+
+
+// 广度优先遍历实现
+var minDepth2 = function(root) {
+  if(!root) return 0;
+
+  const _Queue = new Queue();
+  root.depth = 1;
+  _Queue.push(root);
+
+  let currentNode = _Queue.pop();
+
+  while (currentNode) {
+    if(!currentNode.left && !currentNode.right) {
+      return currentNode.depth;
+    }
+    if(currentNode.left) {
+      // 需要在这里改变depth。队列里面有不同层级的节点
+      currentNode.left.depth = currentNode.depth + 1;
+      _Queue.push(currentNode.left);
+    }
+    if(currentNode.right) {
+      currentNode.right.depth = currentNode.depth + 1;
+      _Queue.push(currentNode.right);
+    }
+
+    currentNode = _Queue.pop();
+  }
+
+  return currentNode.depth;
 };
