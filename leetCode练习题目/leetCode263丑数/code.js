82def3e30a8d3b9bc8009d0992675e13db130d31
柏海辉 <baihaihui@ACA80121.ipt.aol.com>
2019-03-12 14:33:17 +0800

    leetCode

---
+++ leetCode练习题目/leetCode263丑数/code.js
@@ -0,0 +1,35 @@
+const isUgly = function(num) {
+  if(num === 1) return true;
+  if(num === 0) return false;
+
+  const stand = [2, 3, 5];
+
+  if(stand.indexOf(num) !== -1) return true;
+
+  if(num % 2 === 0) {
+    return isUgly(num/2);
+  }
+  if(num % 3 === 0) {
+    return isUgly(num/3);
+  }
+  if(num % 5 === 0) {
+    return isUgly(num/5);
+  }
+
+  return false;
+};
+
+isUgly(0);
+
+const isUgly2 = (num) => {
+  if(num === 0) return false;
+  if(num === 1) return true;
+
+  while(num % 2 === 0) num /= 2;
+  while(num % 3 === 0) num /= 3;
+  while(num % 5 === 0) num /= 5;
+
+  return num === 1;
+}
+
+isUgly2(6);
